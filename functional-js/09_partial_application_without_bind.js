dff9a569ae4c162e431a8dbaaa019da28753eaa3
Chinmi <elyesbarnat42@gmail.com>
2018-02-21 11:28:25 +0100

    Add files via upload

--- functional-js/09_partial_application_without_bind.js
+++ functional-js/09_partial_application_without_bind.js
@@ -1,3 +1,18 @@
 const logger = (namespace) => (...args) => console.log(namespace, ...args)
 
 module.exports = logger
+
+// I didn't Function.apply(...)
+// However it has been validated
+
+// Here's their solution :
+
+/*var slice = Array.prototype.slice
+
+function logger(namespace) {
+  return function() {
+    console.log.apply(console, [namespace].concat(slice.call(arguments)))
+  }
+}
+
+module.exports = logger*/
