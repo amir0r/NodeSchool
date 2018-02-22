a0b20397a8d2c579db6abfb6f940dc4740d83ac9
Chinmi <elyesbarnat42@gmail.com>
2018-02-22 11:47:51 +0100

    Add files via upload

--- curry/call_and_apply.js
+++ curry/call_and_apply.js
@@ -11,5 +11,14 @@ const callAndApply = {
 	}
 }
 
-
 module.exports = callAndApply
+
+// Their solution is too easy : I don't like it because you just call the method that you must to rewrite
+/*var callAndApply = {
+  caller: function (object, method, nameArg, ageArg, tShirtSizeArg) {
+    method.call(object, nameArg, ageArg, tShirtSizeArg)
+  },
+  applier: function (object, method, argumentsArr) {
+    method.apply(object, argumentsArr);
+  }
+}*/
